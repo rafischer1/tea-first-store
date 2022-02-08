3e495c25da346f32f64f9c61b834b2b62022e2fa
Artie Fischer <artie@curetechnologies.us>
2022-02-08 10:43:13 -0700

    feat: initializes homepage layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './components/homepage/homepage.component';
 
 function App() {
   return (
     <div className='App'>
-      <h1>App</h1>
+      <HomePage />
     </div>
   );
 }
