fba0c7951b23bd434decfa1dee626ab90c668cc3
Artie Fischer <artie@curetechnologies.us>
2022-02-08 20:16:13 -0700

    wip: directory loading []sections

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,40 +1,10 @@
 import React from 'react';
 import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.component';
 
 const HomePage = () => (
   <div className='homepage'>
-    <div className='directory-menu'>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>THINGS 1</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>THINGS 2</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>THINGS 3</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>THINGS 4</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>THINGS 5</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-    </div>
+    <Directory />
   </div>
 );
 
