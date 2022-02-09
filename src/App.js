aad06d6c2bf3cb426fee38e2e45dbcd6b19b95ca
Artie Fischer <artie@curetechnologies.us>
2022-02-08 21:13:20 -0700

    wip: note diff b/w router v5 and v6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div className='App'>
+      <Routes switch={true}>
+        <Route exact path='/' component={HomePage} />
+        <Route path='applesauce' component={HomePage} />
+      </Routes>
       <HomePage />
     </div>
   );
