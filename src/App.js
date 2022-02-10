510256b3e903ea84174b083ef39eb3479e2bb85f
Artie Fischer <artie@curetechnologies.us>
2022-02-10 15:19:53 -0700

    feat: google oAuth signs in and out

--- src/App.js
+++ src/App.js
@@ -5,23 +5,46 @@ import HomePage from './pages/homepage/homepage.component';
 import Shop from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div className='App'>
-      <Header />
-      <Routes>
-        <Route exact path='/' element={<HomePage />} />
-        <Route path='shop' element={<Shop />} />
-        <Route path='shop/applesauce' element={<Shop />} />
-        <Route path='shop/selbyville' element={<Shop />} />
-        <Route path='shop/dracula' element={<Shop />} />
-        <Route path='shop/photos' element={<Shop />} />
-        <Route path='shop/releases' element={<Shop />} />
-        <Route path='signin' element={<SignInAndSignUp />} />
-      </Routes>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    };
+  }
+  unsubscribeFromAuth = null;
+  componentDidMount() {
+    // initializes firebases auth subscription
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      console.log('USER:', user);
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount() {
+    // closes firebase auth subscription
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Header currentUser={this.state.currentUser} />
+        <Routes>
+          <Route exact path='/' element={<HomePage />} />
+          <Route path='shop' element={<Shop />} />
+          <Route path='shop/applesauce' element={<Shop />} />
+          <Route path='shop/selbyville' element={<Shop />} />
+          <Route path='shop/dracula' element={<Shop />} />
+          <Route path='shop/photos' element={<Shop />} />
+          <Route path='shop/releases' element={<Shop />} />
+          <Route path='signin' element={<SignInAndSignUp />} />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './header.styles.scss';
 import logo from '../../assets/tea.png';
+import { auth } from '../../firebase/firebase.utils';
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className='header'>
     <Link to='/'>
       <img src={logo} className='logo-container' />
@@ -18,9 +19,15 @@ const Header = () => (
       <Link className='option' to='contact'>
         CONTACT
       </Link>
-      <Link className='option' to='signin'>
-        SIGN IN
-      </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className='option' to='signin'>
+          SIGN IN
+        </Link>
+      )}
     </div>
   </div>
 );
