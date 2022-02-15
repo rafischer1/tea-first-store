9ca0d992dbc2f317512be1c2e4967274c82630ad
Artie Fischer <artie@curetechnologies.us>
2022-02-15 09:43:32 -0700

    feat: cart util helpers

--- src/components/menu-item/shop-data.js
+++ src/components/menu-item/shop-data.js
@@ -55,25 +55,25 @@ const SHOP_DATA = [
     routeName: "selbyville",
     items: [
       {
-        id: 1,
+        id: 8,
         name: "Mansions",
         imageUrl: "https://f4.bcbits.com/img/a1543980671_16.jpg",
         price: 15,
       },
       {
-        id: 2,
+        id: 9,
         name: "Quiet Until the Thaw LP",
         imageUrl: "https://f4.bcbits.com/img/a1748546458_16.jpg",
         price: 20,
       },
       {
-        id: 3,
+        id: 10,
         name: "Plum Puddinger's Voyage EP",
         imageUrl: "https://f4.bcbits.com/img/a3363139976_16.jpg",
         price: 5,
       },
       {
-        id: 4,
+        id: 11,
         name: "Send Selbyville to Disneyworld (Cassette)",
         imageUrl: "https://f4.bcbits.com/img/a2036108834_16.jpg",
         price: 8,
@@ -86,27 +86,27 @@ const SHOP_DATA = [
     routeName: "dracula",
     items: [
       {
-        id: 1,
+        id: 12,
         name: "Dracula Songs (Cassette)",
         imageUrl:
           "https://1.bp.blogspot.com/_-F5oRQdMYYQ/TOLsswn_fbI/AAAAAAAAAA4/s06GB4IU6rE/s200/draculasongscover.jpg",
         price: 5,
       },
       {
-        id: 2,
+        id: 13,
         name: "Dracula Songs II (Cassette)",
         imageUrl:
           "https://thumbnailer.mixcloud.com/unsafe/600x600/extaudio/1/1/3/d/ceee-bc1e-4dd6-9afb-1d35f7d64abc.jpg",
         price: 5,
       },
       {
-        id: 3,
+        id: 14,
         name: "Dracula Songs III (Cassette) (COMING SOON)",
         imageUrl: " https://f4.bcbits.com/img/a3924038074_16.jpg",
         price: 5,
       },
       {
-        id: 4,
+        id: 15,
         name: "Tea First Compilation",
         imageUrl:
           "https://64.media.tumblr.com/tumblr_lle93zg8v21qk629no1_400.jpg",
@@ -120,28 +120,28 @@ const SHOP_DATA = [
     routeName: "releases",
     items: [
       {
-        id: 1,
+        id: 15,
         name: "Mind Wheel - Moses Blake Road",
         imageUrl:
           "https://3.bp.blogspot.com/-NdZvq_Svays/WgpdOTtrhxI/AAAAAAAAAQ4/-60vcmGPWSAjOfukXobfP3Ni8WRvUZzYACLcBGAs/s1600/mosesblakediscface.jpg",
         price: 10,
       },
       {
-        id: 2,
+        id: 16,
         name: "Graham/Drab Pony (Casette)",
         imageUrl:
           "https://64.media.tumblr.com/tumblr_lrgxxjrivU1qk629no1_1280.jpg",
         price: 5,
       },
       {
-        id: 3,
+        id: 17,
         name: "Graham - Grimpen Mire",
         imageUrl:
           "https://3.bp.blogspot.com/_-F5oRQdMYYQ/TOLstY4pZkI/AAAAAAAAAA8/oJvJLktylyc/s200/grimpenmirecover.jpg",
         price: 10,
       },
       {
-        id: 4,
+        id: 18,
         name: "Blind Pelican/Artie Appleseed (Cassette)",
         imageUrl:
           "https://4.bp.blogspot.com/-D7RsBql-zSo/UC0L3t_vnnI/AAAAAAAAAn8/Q917B8virDk/s200/187873731-1.jpg",
@@ -155,42 +155,42 @@ const SHOP_DATA = [
     routeName: "photos",
     items: [
       {
-        id: 1,
+        id: 19,
         name: "Mural Lisbon",
         imageUrl:
           "https://64.media.tumblr.com/b58e2bd78cdb7cf0282295caefc5c88c/tumblr_p2k9j6wJQY1qk629no2_1280.jpg",
         price: 105,
       },
       {
-        id: 2,
+        id: 20,
         name: "Rock (Maine)",
         imageUrl:
           "https://64.media.tumblr.com/7b4c8a354c542b467dc621684a6699e2/tumblr_oiywetdmwh1qk629no1_1280.jpg",
         price: 85,
       },
       {
-        id: 3,
+        id: 21,
         name: "Sheep (Westfjords, Iceland)",
         imageUrl:
           "https://64.media.tumblr.com/1e223d3d4547cd9e14dd2096c5182380/tumblr_oe852evN7a1qk629no1_1280.jpg",
         price: 75,
       },
       {
-        id: 4,
+        id: 22,
         name: "Port Charlotte Cemetery, Islay, Scotland",
         imageUrl:
           "https://64.media.tumblr.com/8a0276ca9f7a1159b6782b7d0ab0758f/tumblr_o4lylpeo4D1qk629no1_1280.jpg",
         price: 210,
       },
       {
-        id: 5,
+        id: 23,
         name: "Scarborough Salt March (Maine)",
         imageUrl:
           "https://64.media.tumblr.com/df57b153db2bb2c203a2bb548b125dac/tumblr_nvtp1hHggv1qk629no1_1280.jpg",
         price: 60,
       },
       {
-        id: 6,
+        id: 24,
         name: "Hillsboro Inlet (Florida)",
         imageUrl:
           "https://64.media.tumblr.com/59c5b39360339805dd9994864c2446b3/tumblr_nl6oaiqrb61qk629no1_1280.jpg",
