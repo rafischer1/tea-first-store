ba08a77127c1988903d3bf0bdea2600c7db01f56
Artie Fischer <artie@curetechnologies.us>
2022-02-21 13:09:40 -0700

    wip: sctrictness enforced

--- src/components/custom-button/custom-button.component.tsx
+++ src/components/custom-button/custom-button.component.tsx
@@ -2,12 +2,18 @@ import React from "react";
 
 import "./custom-button.styles.scss";
 
+type Props = {
+  children: any;
+  isGoogleSignIn: boolean;
+  inverted: boolean;
+};
+
 const CustomButton = ({
   children,
   isGoogleSignIn,
   inverted,
   ...otherProps
-}) => (
+}: Props) => (
   <button
     className={`${inverted} ? "inverted" : "", ${
       isGoogleSignIn ? "google-sign-in" : ""
