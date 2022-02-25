357d7548f44398e6406d65160a59b360d5d9b426
Artie Fischer <artie@curetechnologies.us>
2022-02-24 19:40:51 -0700

    wip: testing aint easy

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,9 @@
+// import { render } from "@testing-library/react";
+// import React from "react";
+// import App from "../App";
+
+it("inits", () => {
+  // const { container } = render(<App />);
+  // console.log(container);
+  // expect(container).toBeCalled();
+});
--- src/__tests__/CustomButton.test.tsx
+++ src/__tests__/CustomButton.test.tsx
@@ -1,42 +1,17 @@
 import React from "react";
-import { render, findByText, findAllByText } from "@testing-library/react";
 import CustomButton from "../components/custom-button/custom-button.component";
-import exp from "constants";
-
-type Props = {
-  children: any;
-  isGoogleSignIn: boolean;
-  inverted: boolean;
-  type?: "button" | "submit" | "reset" | undefined;
-  onClick?: any;
-};
+import { shallow } from "enzyme";
 
 describe("<CustomButton />", () => {
   test("should display a <button>", async () => {
-    const x = renderCustomButton({
-      type: "button",
-      inverted: false,
-      isGoogleSignIn: false,
-      onClick: () => "hi",
-      children: [],
-    });
-    console.log(x.container);
+    expect(
+      shallow(
+        <CustomButton
+          isGoogleSignIn={false}
+          inverted={false}
+          children={"Test Button"}
+        />
+      )
+    ).toHaveTextContent("Test Button");
   });
 });
-
-function renderCustomButton(props: Partial<Props> = {}) {
-  const defaultProps: Props = {
-    type: "button",
-    inverted: false,
-    isGoogleSignIn: false,
-    onClick: () => "hi",
-    children: [],
-  };
-  return render(
-    <CustomButton
-      {...defaultProps}
-      {...props}
-      children={defaultProps.children}
-    />
-  );
-}
