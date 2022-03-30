c149b6ab797ace01d6cfb1d34f21e922a898d647
rafischer1 <artiefischer@gmail.com>
2022-03-30 16:57:16 -0600

    fix: color context

--- src/contexts/color.context.tsx
+++ src/contexts/color.context.tsx
@@ -2,12 +2,13 @@ import React, { createContext, useState } from "react";
 
 export const ColorContext = createContext({
   color: "red",
-  setColor: (val: ColorName) => val,
+  setColor: (color: ColorName) => color,
 });
 
 export const ColorProvider = ({ children }: any) => {
   const [color, setColor] = useState("red");
   const value = { color, setColor };
+
   return (
     // @ts-ignore
     <ColorContext.Provider value={value}>{children}</ColorContext.Provider>
