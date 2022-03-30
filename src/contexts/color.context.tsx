9a54e6f33a08f7a1aa1a4c13fae68b517693d7b7
rafischer1 <artiefischer@gmail.com>
2022-03-29 20:43:42 -0600

    fix: comments

--- src/contexts/color.context.tsx
+++ src/contexts/color.context.tsx
@@ -1,12 +1,10 @@
 import React, { createContext, useState } from "react";
 
-// the user context value to access
 export const ColorContext = createContext({
   color: "red",
   setColor: (val: string) => val,
 });
 
-// the component wrapper for providing context
 export const ColorProvider = ({ children }: any) => {
   const [color, setColor] = useState("red");
   const value = { color, setColor };
