6c79a62dc1a8ef1c339a01e1cb03abb840110ebc
rafischer1 <artiefischer@gmail.com>
2022-04-18 12:38:13 -0600

    fix: const -> enum typing

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -17,15 +17,20 @@ import { RootState } from "../../redux/root-reducer";
 import { signOutStart } from "../../redux/user/user.actions";
 
 const Header = () => {
+  // redux action dispatch
   const dispatch = useDispatch();
+  // context subscription for color selection
   const { color } = useContext(ColorContext);
+  // redux selectors for component state
   const currentUser = useSelector((state: RootState) => state.user.currentUser);
   const cartDropdownHidden = useSelector(
     (state: RootState) => state.cartDropdown.hidden
   );
 
+  // es6 method redux action dispatch
   const signOutUser = () => dispatch(signOutStart());
 
+  // css-in-js styling for Link rendering
   return (
     <HeaderContainer>
       <LogoContainer to="/">
@@ -64,6 +69,7 @@ const Header = () => {
   );
 };
 
+// "Older" style redux mapStateToProps
 // MapStateToProps replaced by useSelector hook
 // const mapStateToProps = createStructuredSelector({
 //   currentUser: selectCurrentUser,
