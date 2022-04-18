fd45dd1c1e0f8f70d551455cfa7b4df462dc86f1
rafischer1 <artiefischer@gmail.com>
2022-04-18 10:43:21 -0600

    spec: app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,23 @@
-// import { render } from "@testing-library/react";
-// import React from "react";
-// import App from "../App";
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { mockStore } from "./mocks/mock-store";
 
-it("inits", () => {
-  // const { container } = render(<App />);
-  // console.log(container);
-  // expect(container).toBeCalled();
+describe("App", () => {
+  let component: any;
+  beforeEach(() => {
+    component = render(
+      <Provider store={mockStore}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    );
+  });
+
+  it("should render App component", () => {
+    expect(component).toBeTruthy();
+  });
 });
