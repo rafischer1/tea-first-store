592c166c7e8dfe72ef15efe392d687d91483c917
rafischer1 <artiefischer@gmail.com>
2023-11-08 16:18:39 -0500

    fetching users data from nest api

--- src/routes/ApiTest/ApiTest.compnent.tsx
+++ src/routes/ApiTest/ApiTest.compnent.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TestContainer from "./api-test.styles";
+
+type User = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  alternateName: string;
+  email: string;
+};
+
 const ApiTest = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4000/users")
+      .then((response) => response.json())
+      .then(setUsers);
+  }, []);
+
   return (
     <TestContainer>
+      <div></div>
       <form className="form">
         <h2>Api Test Form: Users</h2>
         <label style={{ margin: 10 }}>First Name</label>
@@ -21,6 +38,32 @@ const ApiTest = () => {
           Submit
         </button>
       </form>
+      <div style={{ borderWidth: 5, borderColor: "white", padding: 10 }}>
+        {users?.map((user: User) => (
+          <div key={user.id}>
+            <label>
+              First Name:
+              <div>{user.firstName}</div>
+            </label>
+            <br />
+            <label>
+              Last Name:
+              <div>{user.lastName}</div>
+            </label>
+            <br />
+            <label>
+              Alternate Name:
+              <div>{user.alternateName}</div>
+            </label>
+            <br />
+            <label>
+              Email:
+              <div>{user.email}</div>
+            </label>
+            <br />
+          </div>
+        ))}
+      </div>
     </TestContainer>
   );
 };
